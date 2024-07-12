a22d390b65dcd72165ab6e7dcc59ff65bebe03b1
kf-cuanschutz <127144640+kf-cuanschutz@users.noreply.github.com>
2024-07-12 13:34:46 -0600

    Update step0_modify_overlay.sh
    1) Added the "-d" for debug. This is in case a user run into an issue launching the container, it will save time during ticket debugging.
    
    2) Added " --bind /projects,/scratch/alpine,$CURC_CONTAINER_DIR_OOD" as done in the Boulder latest documentation: https://curc.readthedocs.io/en/latest/gateways/OnDemand.html?highlight=rstudio#rstudio

--- Rstudio_related_scripts/step0_modify_overlay.sh
+++ Rstudio_related_scripts/step0_modify_overlay.sh
@@ -1,4 +1,4 @@
 export APPTAINER_TMPDIR=/projects/$USER/.rstudioserver
 export APPTAINER_CACHEDIR=/projects/$USER/.rstudioserver
-apptainer shell --fakeroot --overlay /projects/$USER/.rstudioserver/rstudio-server-4.2.2_overlay.img /curc/sw/containers/open_ondemand/rstudio-server-4.2.2.sif
+apptainer -d shell  --bind /projects,/scratch/alpine,$CURC_CONTAINER_DIR_OOD --fakeroot --overlay /projects/$USER/.rstudioserver/rstudio-server-4.2.2_overlay.img /curc/sw/containers/open_ondemand/rstudio-server-4.2.2.sif
 

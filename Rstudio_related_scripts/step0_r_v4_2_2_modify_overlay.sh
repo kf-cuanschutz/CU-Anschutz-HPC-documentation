35db22d375c8ff11bd2e83fb2240a11d7d176377
kf-cuanschutz <127144640+kf-cuanschutz@users.noreply.github.com>
2024-10-15 14:10:34 -0600

    Create step0_r_v4_2_2_modify_overlay.sh
    Created a file for the version 4.2.2

---
+++ Rstudio_related_scripts/step0_r_v4_2_2_modify_overlay.sh
@@ -0,0 +1,9 @@
+export ALPINE_SCRATCH=/gpfs/alpine1/scratch/$USER
+export APPTAINER_TMPDIR=$ALPINE_SCRATCH/apptainer/tmp
+export APPTAINER_CACHEDIR=$ALPINE_SCRATCH/apptainer/cache
+mkdir -pv $APPTAINER_CACHEDIR $APPTAINER_TMPDIR
+
+export r_app_version="4.2.2"
+apptainer -d shell --bind /projects,/scratch/alpine,$CURC_CONTAINER_DIR_OOD --fakeroot --overlay /projects/$USER/.rstudioserver/rstudio-${r_app_version}/rstudio-server-${r_app_version}_overlay.img /curc/sw/containers/open_ondemand/rstudio-server-${r_app_version}.sif
+
+
